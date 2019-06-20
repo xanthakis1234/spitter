dd03055fa7673e402f5b051ba695ce1ae762967a
Theodoros Stroh <Theodore.Stroh@eurodyn.com>
2019-06-20 14:52:13 +0300

    Amend db script 002

--- database_scripts/spitterdb_002.sql
+++ database_scripts/spitterdb_002.sql
@@ -1,4 +1,5 @@
-create table db_versions (
+create table db_versions
+(
   dbversion   varchar(6)  not null primary key,
   record_date datetime    not null,
   author      varchar(20) not null,
@@ -6,3 +7,9 @@ create table db_versions (
   type        varchar(50) not null
 );
 
+INSERT INTO spitterdb.db_versions (dbversion, record_date, author, reason, type)
+VALUES ('001', '2019-06-20 10:25:51', 'nektarios', 'Create spitter and spittle tables.
+Populate tables above with sample data', 'DDL');
+
+INSERT INTO spitterdb.db_versions (dbversion, record_date, author, reason, type)
+VALUES ('002', '2019-06-20 10:27:35', 'stroh', 'Create dbversion table', 'DDL');
